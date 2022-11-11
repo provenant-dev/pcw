f28b7cf9553cfd5abaf30a3cb30985d94689bdd7
Daniel Hardman <daniel.hardman@gmail.com>
2022-11-11 16:50:54 +0100

    progress
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- maintain.py
+++ maintain.py
@@ -1,57 +1,7 @@
-import os
 import re
-import shutil
-import sys
-import time
 import traceback
 
-import blessings
-
-
-LOG_FILE = os.path.expanduser("~/.maintain.log")
-PATCH_CHECK_FILE = os.path.expanduser("~/.patch-check")
-RESET_PROMPT = """\
-Resetting state is destructive. It removes your history, all your AIDs,
-and all your keys. All credentials you've received or issued become
-unusable, and all multisigs where you are a participant lose your input.
-It is basically like creating a brand new wallet. Type "yes" to confirm."""
-RERUNNER = '.rerun'
-ESC_SEQ_PAT = re.compile("(?:\007|\033)\\[[0-9;]+[Bm]")
-
-log = open(LOG_FILE, 'at')
-term = blessings.Terminal()
-
-
-def cout(txt):
-    sys.stdout.write(txt)
-    sys.stdout.flush()
-    log.write(ESC_SEQ_PAT.sub("", txt))
-    log.flush()
-
-
-def ask(question):
-    cout(term.bright_yellow(question) + '\n   ')
-    cout(term.bright_red + ">> " + term.white)
-    try:
-        answer = input().strip()
-    finally:
-        cout(term.normal)
-    return answer
-
-
-def backup_file(fname, once_only=True):
-    backup_name = fname + '.bak'
-    if not os.path.isfile(backup_name) or not once_only:
-        log.write("Backing up %s to %s.\n" % (fname, backup_name))
-        shutil.copyfile(fname, backup_name)
-
-
-def restore_from_backup(fname):
-    backup_name = fname + '.bak'
-    if os.path.isfile(backup_name):
-        log.write("Restoring %s from %s.\n" % (fname, backup_name))
-        shutil.copyfile(backup_name, fname)
-        return True
+from util import *
 
 
 def fix_prompt(script):
@@ -66,30 +16,9 @@ def fix_prompt(script):
     return '\n'.join(new_lines)
 
 
-def run(cmd):
-    exitcode = os.system(cmd + f" >{LOG_FILE} 2>&1")
-    if exitcode:
-        cout(term.bright_red + "System command exited with code %d. Command was:" + term.normal + "\n  %s\n" % (exitcode, cmd))
-    return exitcode
-
-
-def get_repo_name(url):
-    return url[url.rfind('/') + 1:-4]
-
-
-class TempWorkingDir:
-    def __init__(self, path):
-        self.reset = os.getcwd()
-        self.path = path
-    def __enter__(self):
-        os.chdir(self.path)
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        os.chdir(self.reset)
-
-
 def refresh_repo(url):
     fetched_anything = True
-    repo_name = get_repo_name(url)
+    repo_name = url[url.rfind('/') + 1:-4]
     if os.path.isdir(repo_name):
         cout(f"Checking for {repo_name} updates.\n")
         git_log = os.path.expanduser(f"~/.git-pull-{repo_name}.log")
@@ -106,20 +35,13 @@ def refresh_repo(url):
     return fetched_anything
 
 
-def get_variable(variable, script):
-    var_pat = re.compile(r'^\s*' + variable + r'\s*=\s*"([^"]+)"\n', re.MULTILINE)
-    m = var_pat.search(script)
-    if m:
-        return m.group(1)
-
-
 def personalize():
     bashrc = ".bashrc"
     semaphore = bashrc + '-changed'
     backup_file(bashrc)
     with open(bashrc, 'rt') as f:
         script = f.read()
-    owner = get_variable("OWNER", script)
+    owner = get_shell_variable("OWNER", script)
     if not owner:
         owner = ask("What is your first and last name?")
         script = f'OWNER="{owner}"\n' + fix_prompt(script)
@@ -127,15 +49,6 @@ def personalize():
             f.write(script)
         run(f"touch {semaphore}")
     return owner
-
-
-def time_since(log_file):
-    now = time.time()
-    if os.path.isfile(log_file):
-        elapsed = now - os.stat(log_file).st_mtime
-    else:
-        elapsed = now
-    return elapsed
 
 
 def patch_os(cache_secs=86400):
@@ -228,4 +141,3 @@ def do_maintenance():
 
 if __name__ == '__main__':
    do_maintenance()
-
---
+++ util.py
@@ -0,0 +1,93 @@
+import os
+import shutil
+import sys
+import time
+
+import blessings
+
+
+LOG_FILE = os.path.expanduser("~/.maintain.log")
+PATCH_CHECK_FILE = os.path.expanduser("~/.patch-check")
+RESET_PROMPT = """\
+Resetting state is destructive. It removes your history, all your AIDs,
+and all your keys. All credentials you've received or issued become
+unusable, and all multisigs where you are a participant lose your input.
+It is basically like creating a brand new wallet. Type "yes" to confirm."""
+RERUNNER = '.rerun'
+ESC_SEQ_PAT = re.compile("(?:\007|\033)\\[[0-9;]+[Bm]")
+
+log = open(LOG_FILE, 'at')
+term = blessings.Terminal()
+
+
+class TempWorkingDir:
+    """
+    Changed working directory until python context is reset, then
+    changes back.
+    """
+    def __init__(self, path):
+        self.reset = os.getcwd()
+        self.path = path
+
+    def __enter__(self):
+        os.chdir(self.path)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir(self.reset)
+
+
+def cout(txt):
+    sys.stdout.write(txt)
+    sys.stdout.flush()
+    log.write(ESC_SEQ_PAT.sub("", txt))
+    log.flush()
+
+
+def ask(question):
+    cout(term.bright_yellow(question) + '\n   ')
+    cout(term.bright_red + ">> " + term.white)
+    try:
+        answer = input().strip()
+    finally:
+        cout(term.normal)
+    return answer
+
+
+def run(cmd):
+    exitcode = os.system(cmd + f" >{LOG_FILE} 2>&1")
+    if exitcode:
+        cout(term.bright_red + "System command exited with code %d. Command was:" + term.normal + "\n  %s\n" % (exitcode, cmd))
+    return exitcode
+
+
+def backup_file(fname, once_only=True):
+    backup_name = fname + '.bak'
+    if not os.path.isfile(backup_name) or not once_only:
+        log.write("Backing up %s to %s.\n" % (fname, backup_name))
+        shutil.copyfile(fname, backup_name)
+
+
+def restore_from_backup(fname):
+    backup_name = fname + '.bak'
+    if os.path.isfile(backup_name):
+        log.write("Restoring %s from %s.\n" % (fname, backup_name))
+        shutil.copyfile(backup_name, fname)
+        return True
+
+
+def time_since(file_modified):
+    now = time.time()
+    if os.path.isfile(file_modified):
+        elapsed = now - os.stat(file_modified).st_mtime
+    else:
+        elapsed = now
+    return elapsed
+
+
+def get_shell_variable(variable, script):
+    var_pat = re.compile(r'^\s*' + variable + r'\s*=\s*"([^"]+)"\n', re.MULTILINE)
+    m = var_pat.search(script)
+    if m:
+        return m.group(1)
+
+
