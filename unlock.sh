6b7b17ef1635b22c5a5e28280bd32d37f304e191
Daniel Hardman <daniel.hardman@gmail.com>
2022-12-05 14:36:21 +0100

    fix missing fi in bash script
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- unlock.sh
+++ unlock.sh
@@ -1,13 +1,13 @@
 set -e
 saved_hash=`head -n 1 ~/.passcode-hash`
-if [ "$1" = "--debug" ]; then printf "Saved passcode hash = $saved_hash.\n"
+if [ "$1" = "--debug" ]; then printf "Saved passcode hash = $saved_hash.\n"; fi
 hash=""
 while true
 do
     printf "Enter passcode to unlock wallet: "
     read -s TYPED_PASSCODE
     hash=`echo "$TYPED_PASSCODE" | sha256sum | cut -f1 -d' '`
-    if [ "$1" = "--debug" ]; then printf "Hash of that passcode = $hash.\n"
+    if [ "$1" = "--debug" ]; then printf "Hash of that passcode = $hash.\n"; fi
     if [ "$hash" = "$saved_hash" ]; then
       set +e
       break
