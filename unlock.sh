7c3c68e2849814046cb2d343f25234f9b5a7c8b5
Daniel Hardman <daniel.hardman@gmail.com>
2022-12-06 12:13:40 +0100

    polish unlock feature
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- unlock.sh
+++ unlock.sh
@@ -1,12 +1,13 @@
 saved_hash=`head -n 1 ~/.passcode-hash`
 if [ "$1" = "--debug" ]; then printf "Saved passcode hash = $saved_hash.\n"; fi
 hash=""
+printf "\n"
 while true
 do
     printf "\033[0;34mEnter 21-char passcode to unlock wallet:\033[00m "
     read -s TYPED_PASSCODE
     # Erase previous line.
-    tput cuu1 && tput cuu1 && tput el
+    printf "\n" && tput cuu1 && tput cuu1 && tput el
     hash=`printf "$TYPED_PASSCODE" | sha256sum | cut -f1 -d' '`
     if [ "$1" = "--debug" ]; then printf "Hash of that passcode = $hash.\n"; fi
     if [ "$hash" = "$saved_hash" ]; then
