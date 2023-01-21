ac329076044e92e5f4f469b203254af9698040ca
Daniel Hardman <daniel.hardman@gmail.com>
2023-01-21 18:57:01 +0100

    add script to change person alias
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- set-person.sh
+++ set-person.sh
@@ -1,6 +1,6 @@
 singlesigs=$(python3 ~/pcw/aliases.py subset singlesig)
 if [ $# -lt 2 ]; then
-  read -p "echo 1>&2 "Which person alias ($singlesigs) do you want to activate? " -r person
+  read -p "Which person alias ($singlesigs) do you want to activate? " -r person
 else
   person=$1
 fi
