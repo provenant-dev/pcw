734cb0cf8641a29641a8d5274787a0d80eb10bbd
Daniel Hardman <daniel.hardman@gmail.com>
2023-01-21 19:13:00 +0100

    add script to change person alias
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- shellvar.h
+++
@@ -1,6 +0,0 @@
-import os
-import util
-
-def set_or_update(varname, )
-
-if __name__ == '__main__':
---
+++ shellvar.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from util import set_or_update_shell_variable
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print("shellvar VARIABLE SCRIPT VALUE [export]")
+        sys.exit(1)
+        if len(sys.argv) > 4:
+            export = bool(sys.argv[4].lower()[0] == 'e')
+        else:
+            export = False
+    script = os.path.expanduser(sys.argv[2])
+    if not os.path.isfile(script):
+        print(f"Script {script} does not exist.")
+        sys.exit(1)
+    with open(script, "rt") as f:
+        txt = f.read()
+    new_txt = set_or_update_shell_variable(sys.argv[1], script, sys.arg[3], export)
+    os.system(f"cp {fname} {fname}.bak")
+    with open(script, "wt") as f:
+        f.write(new_txt)
