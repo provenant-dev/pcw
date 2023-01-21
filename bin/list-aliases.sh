5f2dc053e004994b8b28a720edac1b6b36f0ecdc
Daniel Hardman <daniel.hardman@gmail.com>
2023-01-21 15:23:07 +0100

    fix conditional
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- bin/list-aliases.sh
+++ bin/list-aliases.sh
@@ -9,7 +9,7 @@ if len(sys.argv) != 1:
 
 passcode = os.environ.get('TYPED_PASSCODE', None)
 db_name = os.environ.get('WALLET_DB_NAME', None)
-if passcode && db_name:
+if passcode and db_name:
     os.system(f'kli list --name {db_name} --passcode "{passcode}"')
 else:
     print('Wallet passcode is not in memory. Run "unlock" command first.')
