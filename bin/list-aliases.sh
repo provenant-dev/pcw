2a64e479d026154432ed312f006b9682ea1b084f
Daniel Hardman <daniel.hardman@gmail.com>
2023-01-21 15:27:37 +0100

    improve alias handling
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- bin/list-aliases.sh
+++ bin/list-aliases.sh
@@ -8,9 +8,8 @@ if len(sys.argv) != 1:
     sys.exit(1)
 
 passcode = os.environ.get('TYPED_PASSCODE', None)
-db_name = os.environ.get('WALLET_DB_NAME', None)
-if passcode and db_name:
-    os.system(f'kli list --name {db_name} --passcode "{passcode}"')
+if passcode:
+    os.system(f'kli list --name XAR --passcode "{passcode}"')
 else:
     print('Wallet passcode is not in memory. Run "unlock" command first.')
     sys.exit(1)
