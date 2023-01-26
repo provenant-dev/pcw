28f7292ce25846a3032cb763a5d07feaeb276679
Daniel Hardman <daniel.hardman@gmail.com>
2023-01-26 14:27:18 +0100

    experiment with automatic shutdown
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

---
+++ shutdown-if-inactive.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# Shuts down the wallet host if it has no active SSH session for a while.
+# Wallet can be restarted by curling https://start.wallet.provenant.net/user@email.
+#
+# This script is designed to be executed as root from a cron job. It will power
+# off the host on the 2nd consecutive run without an active ssh session, or if
+# the last ssh session was closed more than 30 minutes ago. That prevents an
+# undesirable shutdown when the machine was just started, or on a brief disconnect.
+#
+# To enable, add this entry to /etc/crontab:
+# */30 *   * * *     /shutdown-if-inactive.sh >> /var/log/shutdown-if-inactive.log 2>&1
+#
+
+MARKER_FILE="/tmp/ssh-inactive-flag"
+
+STATUS=$(netstat | grep ssh | grep ESTABLISHED &>/dev/null)
+if [ $? -eq 0 ]; then
+  STATUS="active"
+  echo $(date) ": active SSH connection"
+else
+  echo $(date) ": no active SSH connection"
+  STATUS="inactive"
+fi
+
+if [ "$STATUS" == "inactive" ]; then
+  if [ -f "$MARKER_FILE" ]; then
+
+    # If someone logged in within the last 30 minutes, don't shut down.
+    if ! last --since "30 minutes ago" | grep -q "pts/"; then
+      echo $(date) ": Shutting down due to no ssh activity for 30 minutes."
+      poweroff
+    fi
+
+  else
+    # Create a marker file so that it will shut down if still inactive on the next time this script runs.
+    touch "$MARKER_FILE"
+  fi
+else
+  # Delete marker file if it exists
+  rm --force "$MARKER_FILE"
+fi
