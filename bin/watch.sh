a0df4544612bd03ace9e2bcf941b9736bb78dd07
Daniel Hardman <daniel.hardman@gmail.com>
2023-02-02 16:08:47 +0100

    improve watch command
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- bin/watch.sh
+++ bin/watch.sh
@@ -1,6 +1,7 @@
-# Make sure wallet is initialized.
-pkli list >/dev/null &2>1
-if [ $? -ne 0 ]; then
+# Watching witnesses only makes sense if we actually have AIDs defined.
+aliases="$(pkli list 2>&1)"
+regex="eystore must already exist"
+if [[ "$aliases" =~ $regex ]]; then
   printf "Your wallet is empty. First step is to run create-local-aid.\n"
 else
   printf "Syncing wallet with witnesses.\n\n" && pkli local watch
