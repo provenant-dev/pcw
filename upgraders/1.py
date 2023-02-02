65c5e36f9f73b79742bf2562d1db01e1800c1330
Daniel Hardman <daniel.hardman@gmail.com>
2023-02-02 23:53:56 +0100

    fix upgraders/1 in case there's no data
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- upgraders/1.py
+++ upgraders/1.py
@@ -7,7 +7,8 @@ if os.path.isdir(folder):
     items = [x for x in os.listdir(folder) if x != '.do_not_delete']
     if items:
         run_or_die(f"mv ~/xar/data {temp_location}")
-        run_or_die(f"rm -rf ~/xar")
-        run_or_die(f"git clone https://github.com/provenant-dev/vlei-qvi.git xar")
+    run_or_die(f"rm -rf ~/xar")
+    run_or_die(f"git clone https://github.com/provenant-dev/vlei-qvi.git xar")
+    if items:
         run_or_die(f"cp -r {temp_location}/* ~/xar/data")
         run_or_die(f"rm -rf {temp_location}")
