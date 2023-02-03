d52d66e484499534ec2aa8c1a169e36c1f3f0fd0
Daniel Hardman <daniel.hardman@gmail.com>
2023-02-03 01:04:01 +0100

    fix unlock.sh logic to handle hardcoded passcode better
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- upgraders/1.py
+++ upgraders/1.py
@@ -1,6 +1,10 @@
 from helpers import *
 
 os.chdir(HOME_FOLDER)
+salt_file = os.path.join(XAR_FOLDER, "salt")
+salt_bak_file = os.path.join(HOME_FOLDER, '.salt.bak')
+if os.path.isfile(salt_file):
+    run_or_die(f"cp {salt_file} {salt_bak_file}")
 folder = os.path.join(XAR_FOLDER, 'data')
 temp_location = "~/was-xar-data"
 if os.path.isdir(folder):
@@ -12,3 +16,5 @@ if os.path.isdir(folder):
     if items:
         run_or_die(f"cp -r {temp_location}/* ~/xar/data")
         run_or_die(f"rm -rf {temp_location}")
+if os.path.isfile(salt_bak_file):
+    run_or_die(f"mv {salt_bak_file} {salt_file}")
