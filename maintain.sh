50af01f28bbffc82fcba4ab08c895c06a589af74
Daniel Hardman <daniel.hardman@gmail.com>
2023-02-07 15:50:17 +0100

    add whatsnew feature
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- maintain.sh
+++ maintain.sh
@@ -4,7 +4,7 @@ alias unlock='source ~/pcw/unlock.sh'
 alias lock='unset TYPED_PASSCODE && printf "Wallet locked. Run \"unlock\" to make kli usable again.\n"'
 alias set-person='source ~/pcw/set-person.sh'
 alias set-org='source ~/pcw/set-org.sh'
-alias whatsnew='head -n 20 ~/pcw/whatsnew.md && printf "\n\n(Use less ~/pcw/whatsnew.md for full info.)\n"'
+alias whatsnew='head -n 10 ~/pcw/whatsnew.md && printf "\n\n(Use less ~/pcw/whatsnew.md for full info.)\n"'
 
 # Run the wallet maintenance script at least once, and
 # up to 3 times to account for relaunches for self patching.
