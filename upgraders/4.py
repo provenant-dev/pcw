1d7daa75850252b5f2f6f50659b5100712168910
Daniel Hardman <daniel.hardman@gmail.com>
2023-02-07 13:12:59 +0100

    improve s3 sharing
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- upgraders/4.py
+++ upgraders/4.py
@@ -8,24 +8,26 @@ bashrc = os.path.join(HOME_FOLDER, '.bashrc')
 with open(bashrc, 'rt') as f:
     txt = f.read()
 
-val, exported, start, end = get_shell_variable("S3_SECRET_ACCESS_KEY", txt)
-if end > 0:
-    txt = txt[0:start].rstrip() + "\n" + txt[end:].lstrip()
+if "PCW_SHARE_S3_ACCESS_KEY_ID" not in txt:
 
-val, exported, start, end = get_shell_variable("S3_ACCESS_KEY_ID", txt)
-if end > 0:
-    txt = txt[0:start].rstrip() + "\n" + txt[end:].lstrip()
+    val, exported, start, end = get_shell_variable("S3_SECRET_ACCESS_KEY", txt)
+    if end > 0:
+        txt = txt[0:start].rstrip() + "\n" + txt[end:].lstrip()
 
-txt = txt.rstrip() + """
+    val, exported, start, end = get_shell_variable("S3_ACCESS_KEY_ID", txt)
+    if end > 0:
+        txt = txt[0:start].rstrip() + "\n" + txt[end:].lstrip()
 
-# Used by share-via-s3 to access a public bucket. This is low security
-# and low stakes, primarily used as a convenient transfer method.
-export PCW_SHARE_S3_ACCESS_KEY_ID="AKIAY26NZEFT73QNUWGJ"
-export PCW_SHARE_S3_ACCESS_KEY="/W6bv5C5hymUoqLc8s8EqLYcI5UMXHjQPuvSvLJx"
-"""
+    txt = txt.rstrip() + """
+    
+    # Used by share-via-s3 to access a public bucket. This is low security
+    # and low stakes, primarily used as a convenient transfer method.
+    export PCW_SHARE_S3_ACCESS_KEY_ID="AKIAY26NZEFT73QNUWGJ"
+    export PCW_SHARE_S3_ACCESS_KEY="/W6bv5C5hymUoqLc8s8EqLYcI5UMXHjQPuvSvLJx"
+    """
 
-with open(bashrc, 'wt') as f:
-    f.write(txt)
+    with open(bashrc, 'wt') as f:
+        f.write(txt)
 
-print("\033[0;37mYour environment variables have been updated.\n")
-print("Please run \033[0;33m source ~/.bashrc\033[00m or log in again to activate the change.\n")
+    sys.stderr.write("\033[0;37mYour environment variables have been updated.\n")
+    sys.stderr.write("Please run \033[0;33m source ~/.bashrc\033[00m or log in again to activate the change.\n")
