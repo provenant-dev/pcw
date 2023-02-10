30c6c611f45059289f078d4f597f75291f8384b2
Daniel Hardman <daniel.hardman@gmail.com>
2023-02-10 16:15:41 +0100

    change URL for help
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- upgraders/6.py
+++ upgraders/6.py
@@ -1,4 +1,4 @@
 import os
 
 # Change URL for PCW help.
-os.system('sudo cp /home/ubuntu/pcw/10-help-text /etc/update-motd.d/ && chmod +x /etc/update-motd.d')
+os.system('sudo cp /home/ubuntu/pcw/10-help-text /etc/update-motd.d/ && chmod +x /etc/update-motd.d/10-help-text')
